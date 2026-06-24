4ddf53558b60af358abb3f2af9eb0ce9140af087
nbonpa-a11y <nbonpa@gmail.com>
2026-06-24 09:41:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -71,10 +71,10 @@ function updateResults() {
   errorMessage.textContent = "";
 
   const { winPoints, losePoints } = calculatePoints(winStreak, myPoints, opponentPoints);
-  winResult.value = String(winPoints);
-  loseResult.value = String(losePoints);
-  winAfterResult.value = String(myPoints + winPoints);
-  loseAfterResult.value = String(myPoints + losePoints);
+  winResult.value = `${winPoints}pt`;
+  loseResult.value = `${losePoints}pt`;
+  winAfterResult.value = `${myPoints + winPoints}pt`;
+  loseAfterResult.value = `${myPoints + losePoints}pt`;
 }
 
 form.addEventListener("input", updateResults);
